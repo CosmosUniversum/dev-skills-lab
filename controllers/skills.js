c5bf88d1579152114bdd623b9e957d76233698a7
Josh Castillo <John.Joshua.Castillo@gmail.com>
2021-10-27 22:21:14 -0400

    add create and delete functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -18,8 +18,28 @@ function show(req, res) {
     })
   })
 }
+function newSkill(req, res) {
+  res.render('skills/new')
+}
+
+function create(req, res) {
+  skillDb.create(req.body, function(error, skill){
+    skill,
+    error
+  })
+  res.redirect('skills')
+}
+
+function deleteSkill(req, res) {
+  skillDb.findByIdAndDelete(req.params.id, function(error, skill) {
+    res.redirect('/skills')
+  })
+}
 
 export {
   index,
   show,
+  create,
+  newSkill as new,
+  deleteSkill as delete
 }
